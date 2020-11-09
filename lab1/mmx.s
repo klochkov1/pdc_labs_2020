8d14ac2318c592ac11d6ce97d9bc6179e6ccba53
Aleksandr Klochkov <klochkov1w@gmail.com>
2020-11-09 23:21:34 +0200

    lab1 init commit

---
+++ lab1/mmx.s
@@ -0,0 +1,55 @@
+.data
+
+array:
+	.byte 28, 28, 224, 47, 90, 90, 78, 1, 88, 12, 124, 254, 17, 58, 17, 17, 38, 1, 0, 1, 1, 77, 254, 254, 254, 254, 48, 0, 0, 0, 0, 0
+array_end:
+
+array_size:
+	.quad 27
+leading_one:
+    .byte 255, 0,0,0,0,0,0,0
+all_ones:
+    .quad 0xFFFFFFFFFFFFFFFF
+ 
+.global _start
+
+.section .text
+
+_start:
+	mov		$1, %rax      		# write
+	mov		$1, %rdi      		# file descriptor (stdout)
+	mov		$array, %rsi  		# array pointer
+	mov		(array_size), %rdx	# array size   
+	syscall
+
+    movq    $array, %r8         # array start
+    movq    $4, %rax            # iterator
+
+loop:
+    movq    (%r8), %mm0         # next eight elms to mm0
+    movq    1(%r8), %mm1        # next eight elms shited by 1 el to mm1
+    movq    %mm1, %mm2          # copy mm1 to mm2
+
+    pcmpeqb %mm0, %mm2          # compare mm0 and mm2
+    pxor    (all_ones), %mm2    # invert compare result
+    pand    %mm2, %mm0          # zero dublicates in mm0
+    pand    %mm2, %mm1          # zero dublicates in mm1
+    psllq   $8, %mm1            # shift mm1 by one elem back
+    por     (leading_one), %mm1 # extend shift with ones
+    pand    %mm1, %mm0          # and to get final result for elms
+    movq    %mm0, (%r8)         # populate result to array
+    addq    $8, %r8             # move to next 8 elements
+    decq    %rax                # decrement iterator
+    jnz     loop                # continue if iterator not zero
+
+exit:
+	mov		$1, %rax      		# write
+	mov		$1, %rdi      		# file descriptor (stdout)
+	mov		$array, %rsi  		# array pointer
+	mov		(array_size), %rdx	# array size   
+	syscall
+	
+	mov     $60, %rax			# exit
+	xor     %rdi, %rdi			# zero rdi
+	syscall
+	
---
+++ lab1/plain.s
@@ -0,0 +1,55 @@
+.data
+array:
+	.byte 28, 28, 224, 47, 90, 90, 78, 1, 88, 12, 124, 254, 17, 58, 17, 17, 38, 1, 0, 1, 1, 77, 254, 254, 48, 48, 48
+array_end:
+
+array_size:
+	.quad 27
+ 
+.global _start
+
+.section .text  
+
+_start:
+	mov		$1, %rax      		# write
+	mov		$1, %rdi      		# file descriptor (stdout)
+	mov		$array, %rsi  		# array pointer
+	mov		(array_size), %rdx	# array size
+	syscall	
+
+	mov     $0, %r10	  		# bool flag
+	movq	$array+1, %r9 		# starting from 2nd arrays element
+
+loop_start:    	
+	movb    (%r9), %al	  		# currnet element to al
+	movb    -1(%r9), %bl  		# priv element to bl
+	cmpb	%al, %bl			# check if current and priv are equal
+	jne		check_flag    		# skip zeroing if elements not match
+
+zero_priv:	
+	movb 	$0, -1(%r9)   		# zero priv element if it has dublicate
+	mov 	$1, %r10	  		# flag indicating that current element has dublicate
+	jmp		next_iteration	
+
+check_flag:	
+	cmp 	 $1, %r10	  		# check flag if priv element already had dublicate
+	jne		next_iteration	  	# skip if not
+	movb    $0, -1(%r9)   		# zero priv element
+	mov  	$0, %r10	  		# uncheck flag
+
+next_iteration:	
+	addq  	$1, %r9				# increment currnet element
+	cmpq  	$array_end, %r9 	# check current element adress and array_end address
+	jne    	loop_start      	# got to the start of the loop
+
+exit:
+	mov		$1, %rax      		# write
+	mov		$1, %rdi      		# file descriptor (stdout)
+	mov		$array, %rsi  		# array pointer
+	mov		(array_size), %rdx	# array size   
+	syscall
+	
+	mov     $60, %rax			# exit
+	xor     %rdi, %rdi			# zero rdi
+	syscall
+	
